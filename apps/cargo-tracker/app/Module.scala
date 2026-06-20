f3b35917a2bce143a9c319d29f884f763b6a2f59
k2works <kakimomokuri@gmail.com>
2026-06-20 19:53:35 +0900

    feat(auth): ログイン・ログアウトの画面と AuthController を実装

--- apps/cargo-tracker/app/Module.scala
+++ apps/cargo-tracker/app/Module.scala
@@ -1,14 +1,21 @@
+import cargotracker.auth.domain.UserRepository
+import cargotracker.auth.infrastructure.ScalikeJdbcUserRepository
 import com.google.inject.AbstractModule
 import play.api.db.DBApi
 import scalikejdbc.{ConnectionPool, DataSourceConnectionPool}
 
+import java.time.Clock
 import javax.inject.{Inject, Singleton}
 
 /** Play（HikariCP）のデータソースを ScalikeJDBC のコネクションプールとして登録する。 */
 @Singleton
 class ScalikeJdbcInitializer @Inject() (dbApi: DBApi):
-  ConnectionPool.singleton(new DataSourceConnectionPool(dbApi.database("default").dataSource))
+  ConnectionPool.singleton(
+    new DataSourceConnectionPool(dbApi.database("default").dataSource)
+  )
 
 class Module extends AbstractModule:
   override def configure(): Unit =
     bind(classOf[ScalikeJdbcInitializer]).asEagerSingleton()
+    bind(classOf[UserRepository]).to(classOf[ScalikeJdbcUserRepository])
+    bind(classOf[Clock]).toInstance(Clock.systemUTC())
---
+++ apps/cargo-tracker/app/cargotracker/auth/interfaces/web/AuthController.scala
@@ -0,0 +1,68 @@
+package cargotracker.auth.interfaces.web
+
+import cargotracker.auth.domain.UserRepository
+import play.api.data.Form
+import play.api.data.Forms.*
+import play.api.i18n.I18nSupport
+import play.api.mvc.*
+
+import java.time.{Clock, Instant}
+import javax.inject.{Inject, Singleton}
+
+/** ログイン・ログアウトのコントローラ。 */
+@Singleton
+class AuthController @Inject() (
+    cc: ControllerComponents,
+    userRepository: UserRepository,
+    clock: Clock
+) extends AbstractController(cc)
+    with I18nSupport:
+
+  private case class LoginForm(username: String, password: String)
+
+  private val loginForm: Form[LoginForm] = Form(
+    mapping(
+      "username" -> nonEmptyText(maxLength = 50),
+      "password" -> nonEmptyText(maxLength = 100)
+    )(LoginForm.apply)(lf => Some((lf.username, lf.password)))
+  )
+
+  /** ログインフォーム表示 */
+  def loginPage(): Action[AnyContent] = Action { implicit request =>
+    Ok(views.html.auth.login(loginForm, errorMessage = None))
+  }
+
+  /** ログイン実行（PRG） */
+  def login(): Action[AnyContent] = Action { implicit request =>
+    loginForm
+      .bindFromRequest()
+      .fold(
+        formWithErrors =>
+          BadRequest(
+            views.html.auth
+              .login(formWithErrors, errorMessage = Some("入力内容を確認してください"))
+          ),
+        data =>
+          userRepository.findByUsername(data.username) match
+            case Some(user) if user.authenticate(data.password) =>
+              val rolesStr = user.roles.map(_.toString).mkString(",")
+              Redirect("/")
+                .withSession(
+                  "username" -> user.username,
+                  "roles" -> rolesStr,
+                  "lastAccessedAt" -> Instant.now(clock).toString
+                )
+            case _ =>
+              Unauthorized(
+                views.html.auth.login(
+                  loginForm,
+                  errorMessage = Some("ユーザー名またはパスワードが正しくありません")
+                )
+              )
+      )
+  }
+
+  /** ログアウト */
+  def logout(): Action[AnyContent] = Action { implicit request =>
+    Redirect("/login").withNewSession
+  }
